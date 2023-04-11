07d2ffc444621bfedca999429e5eacc9f8791e26
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-04-11 21:03:51 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require("cors")
  
+server.use(cors())
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
